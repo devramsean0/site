9b3cc2cd7272d710a209beb290598798b4b1082d
Devramsean0 <81807361+devramsean0@users.noreply.github.com>
2023-01-15 15:30:29 +0000

    Some stuff

--- src/root.tsx
+++ src/root.tsx
@@ -1,41 +1,32 @@
 // @refresh reload
-import { Suspense } from "solid-js";
-import {
-  A,
-  Body,
-  ErrorBoundary,
-  FileRoutes,
-  Head,
-  Html,
-  Meta,
-  Routes,
-  Scripts,
-  Title,
-} from "solid-start";
-import "./root.css";
-
+import { Suspense } from 'solid-js';
+import { A, Body, ErrorBoundary, FileRoutes, Head, Html, Meta, Routes, Scripts, Title } from 'solid-start';
+import './css/theme.css';
 export default function Root() {
-  return (
-    <Html lang="en">
-      <Head>
-        <Title>SolidStart - With MDX</Title>
-        <Meta charset="utf-8" />
-        <Meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <Body>
-        <ErrorBoundary>
-          <A href="/">Index</A>
-          <A href="/about">About</A>
-          <Suspense>
-            <main>
-              <Routes>
-                <FileRoutes />
-              </Routes>
-            </main>
-          </Suspense>
-        </ErrorBoundary>
-        <Scripts />
-      </Body>
-    </Html>
-  );
+	return (
+		<Html lang="en">
+			<Head>
+				<Title>Sean Outram</Title>
+				<Meta charset="utf-8" />
+				<Meta name="viewport" content="width=device-width, initial-scale=1" />
+			</Head>
+			<Body>
+				<ErrorBoundary>
+					<A href="/">Index</A>
+					<A href="/about">About</A>
+					<Suspense>
+						<main>
+							<div class="container">
+								<Routes>
+									<FileRoutes />
+								</Routes>
+								&copy; {new Date().getFullYear()} Sean Outram. All rights reserved.
+							</div>
+						</main>
+					</Suspense>
+				</ErrorBoundary>
+				<Scripts />
+			</Body>
+		</Html>
+	);
 }
